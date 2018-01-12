c2a86c9d5e763723abb52a603614bfa39d2ddde4
Manoel Vilela <manoel_vilela@engineer.com>
2018-01-12 03:38:57 -0300

    Change keybinding for 'hy-shell-start-or-switch-to-shell (#63)

--- hy-mode.el
+++ hy-mode.el
@@ -1530,7 +1530,7 @@ Not all defuns can be argspeced - eg. C defuns.\"
 
 ;; Spacemacs users please see spacemacs-hy, all bindings defined there
 (set-keymap-parent hy-mode-map lisp-mode-shared-map)
-(define-key hy-mode-map (kbd "C-c C-e") 'hy-shell-start-or-switch-to-shell)
+(define-key hy-mode-map (kbd "C-c C-z") 'hy-shell-start-or-switch-to-shell)
 (define-key hy-mode-map (kbd "C-c C-b") 'hy-shell-eval-buffer)
 
 (define-key hy-mode-map (kbd "C-c C-t") 'hy-insert-pdb)
